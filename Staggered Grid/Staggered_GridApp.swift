63b95ccbabf8281d87894c1879c016b0a469ef70
Abdul Wahab Khan <abdul.wahab@danatonline.com>
2021-11-13 00:46:16 +0500

    Initial Commit

---
+++ Staggered Grid/Staggered_GridApp.swift
@@ -0,0 +1,17 @@
+//
+//  Staggered_GridApp.swift
+//  Staggered Grid
+//
+//  Created by AWKhan on 13/11/2021.
+//
+
+import SwiftUI
+
+@main
+struct Staggered_GridApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
